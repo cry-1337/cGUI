f9487627aae80341503ae3f318e1e1a2474d1422
cry-1337 <146534003+cry-1337@users.noreply.github.com>
2025-12-15 21:07:42 +0700

    MeshRenderContext

---
+++ cGUI.Render.Contexts/MeshRenderContext.cs
@@ -0,0 +1,25 @@
+﻿using cGUI.Render.Abstraction;
+using System.Collections.Generic;
+
+namespace cGUI.Render.Contexts;
+
+public readonly struct MeshRenderContext() : IMeshRenderContext
+{
+    public readonly int VerticiesCount => Vertices.Count;
+
+    public readonly int IndiciesCount => Indicies.Count;
+
+    public readonly List<Vertex> Vertices => new(12);
+
+    public readonly List<int> Indicies => new(16);
+
+    public readonly void AddIndex(int index) => Indicies.Add(index);
+
+    public readonly void AddVertex(Vertex vertex) => Vertices.Add(vertex);
+
+    public readonly void Clear()
+    {
+        Indicies.Clear(); 
+        Vertices.Clear();
+    }
+}
