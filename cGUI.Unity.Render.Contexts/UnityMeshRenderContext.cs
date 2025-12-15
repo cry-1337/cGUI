dcc3b1f9dd123b843291e32ab91ffff38806a64a
cry-1337 <146534003+cry-1337@users.noreply.github.com>
2025-12-15 21:06:59 +0700

    readonly struct UnityMeshRenderContext

--- cGUI.Unity.Render.Contexts/UnityMeshRenderContext.cs
+++ cGUI.Unity.Render.Contexts/UnityMeshRenderContext.cs
@@ -4,25 +4,25 @@ using System.Collections.Generic;
 
 namespace cGUI.Unity.Render.Contexts;
 
-public struct UnityMeshRenderContext() : IUnityMeshRenderContext
+public readonly struct UnityMeshRenderContext() : IUnityMeshRenderContext
 {
     public readonly List<IUnityMeshData> Meshes => new(2);
 
     public readonly int MeshCount => Meshes.Count;
 
-    public int VerticiesCount => Vertices.Count;
+    public readonly int VerticiesCount => Vertices.Count;
 
-    public int IndiciesCount => Indicies.Count;
+    public readonly int IndiciesCount => Indicies.Count;
 
     public readonly List<Vertex> Vertices => new(16);
 
     public readonly List<int> Indicies => new(12);
 
-    public void AddIndex(int index) => Indicies.Add(index);
+    public readonly void AddIndex(int index) => Indicies.Add(index);
 
-    public void AddVertex(Vertex vertex) => Vertices.Add(vertex);
+    public readonly void AddVertex(Vertex vertex) => Vertices.Add(vertex);
 
-    public void Clear()
+    public readonly void Clear()
     {
         Meshes.Clear();
         Vertices.Clear();
