a15b165bea87678f58f61d9e116f62fefee28843
cry-1337 <146534003+cry-1337@users.noreply.github.com>
2025-12-15 21:04:22 +0700

    refactoring

--- cGUI.Unity.Render.Contexts/UnityMeshRenderContext.cs
+++ cGUI.Unity.Render.Contexts/UnityMeshRenderContext.cs
@@ -4,19 +4,19 @@ using System.Collections.Generic;
 
 namespace cGUI.Unity.Render.Contexts;
 
-public struct UnityQuadRenderContext() : IUnityMeshRenderContext
+public struct UnityMeshRenderContext() : IUnityMeshRenderContext
 {
-    public List<IUnityMeshData> Meshes => new(2);
+    public readonly List<IUnityMeshData> Meshes => new(2);
 
-    public int MeshCount => Meshes.Count;
+    public readonly int MeshCount => Meshes.Count;
 
     public int VerticiesCount => Vertices.Count;
 
     public int IndiciesCount => Indicies.Count;
 
-    public List<Vertex> Vertices => new(16);
+    public readonly List<Vertex> Vertices => new(16);
 
-    public List<int> Indicies => new(12);
+    public readonly List<int> Indicies => new(12);
 
     public void AddIndex(int index) => Indicies.Add(index);
 
