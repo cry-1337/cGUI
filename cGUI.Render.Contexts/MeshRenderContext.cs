1edec7f3c9e8f04cf2c21a1c084b91097c7a2d4b
cry-1337 <146534003+cry-1337@users.noreply.github.com>
2026-01-08 03:35:59 +0700

    refactoring

--- cGUI.Render.Contexts/MeshRenderContext.cs
+++ cGUI.Render.Contexts/MeshRenderContext.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 
 namespace cGUI.Render.Contexts;
 
-public struct MeshRenderContext() : IMeshRenderContext
+public readonly struct MeshRenderContext() : IMeshRenderContext
 {
     private readonly List<Vertex> m_Vertices = new(12);
     private readonly List<int> m_Indices = new(16);
 
-    public List<Vertex> Vertices => m_Vertices;
-    public List<int> Indicies => m_Indices;
+    public readonly List<Vertex> Vertices => m_Vertices;
+    public readonly List<int> Indicies => m_Indices;
 
     public readonly int VerticiesCount => m_Vertices.Count;
     public readonly int IndiciesCount => m_Indices.Count;
